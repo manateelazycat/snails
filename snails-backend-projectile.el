3246a9ff8ddbf16da20c52e50bc47acc6d097091
Andy Stewart <lazycat.manatee@gmail.com>
2019-08-07 05:53:14 +0800

    Wrap standard template.

--- snails-backend-projectile.el
+++ snails-backend-projectile.el
@@ -1,28 +1,102 @@
 ;;; snails-backend-projectile.el --- Projectile backend for snails
 
+;; Filename: snails-backend-projectile.el
+;; Description: Projectile backend for snails
+;; Author: Cosven <http://cosven.me>
+;; Maintainer: Cosven <http://cosven.me>
+;; Copyright (C) 2019, Andy Stewart, all rights reserved.
+;; Created: 2019-08-07 05:50:41
+;; Version: 0.1
+;; Last-Updated: 2019-08-07 05:50:41
+;;           By: Cosven
+;; URL: http://www.emacswiki.org/emacs/download/snails-backend-projectile.el
+;; Keywords:
+;; Compatibility: GNU Emacs 26.2
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 ;;; Commentary:
 ;;
 ;; Projectile backend for snails
+;;
 
-;;; Requires
+;;; Installation:
+;;
+;; Put snails-backend-projectile.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'snails-backend-projectile)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET snails-backend-projectile RET
+;;
+
+;;; Change log:
+;;
+;; 2019/08/07
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
 (require 'snails-core)
+
+
 ;;; Code:
 
-
-(defun snails-backend-projectile--project-root ()
+(defun snails-backend-projectile-project-root ()
   "Find projectile root."
   (let ((filename (buffer-file-name snails-start-buffer)))
     (when filename
       (projectile-project-root (file-name-directory filename)))))
 
-
 (defun snails-backend-projectile-candidates ()
   "List project files."
   (when (featurep 'projectile)
-    (let ((project-root (snails-backend-projectile--project-root)))
+    (let ((project-root (snails-backend-projectile-project-root)))
       (when project-root
         (projectile-project-files project-root)))))
-
 
 (snails-create-sync-backend
  :name
@@ -43,9 +117,8 @@
 
  :candiate-do
  (lambda (candidate)
-  (let ((project-root (snails-backend-projectile--project-root)))
-   (find-file (expand-file-name candidate project-root)))))
-
+   (let ((project-root (snails-backend-projectile-project-root)))
+     (find-file (expand-file-name candidate project-root)))))
 
 (provide 'snails-backend-projectile)
 
