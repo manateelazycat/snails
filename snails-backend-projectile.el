7f1fc86524ac659f03897b542e2e34fca882c6d1
cosven <yinshaowen241@gmail.com>
2019-08-06 20:21:30 +0800

    more robust

--- snails-backend-projectile.el
+++ snails-backend-projectile.el
@@ -10,18 +10,29 @@
 
 ;;; Code:
 
+(defun snails-backend-projectile-files ()
+  "List project files."
+  (let ((filename (buffer-file-name snails-start-buffer)))
+    (when filename
+      (let ((project-root (projectile-project-root (file-name-directory filename))))
+        (when project-root
+           (projectile-project-files project-root))))))
+
+
 (snails-create-sync-backend
  :name
  "PROJECTILE"
 
  :candidate-filter
  (lambda (input)
-   (let (candidates)
-     (dolist (file (projectile-current-project-files))
-       (when (or
-              (string-equal input "")
-              (snails-match-input-p input file))
-         (snails-add-candiate 'candidates (snails-wrap-file-icon file) file)))
+   (let ((candidates)
+         (project-files (snails-backend-projectile-files)))
+     (when project-files
+       (dolist (file project-files)
+         (when (or
+                (string-equal input "")
+                (snails-match-input-p input file))
+           (snails-add-candiate 'candidates (snails-wrap-file-icon file) file))))
      (snails-sort-candidates input candidates 1 1)
      candidates))
 
