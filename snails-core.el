4677daf80a8ee6d5202b98d1acde7a99c2294ec9
Andy Stewart <lazycat.manatee@gmail.com>
2019-08-29 11:59:28 +0800

    Fix subr depend.

--- snails-core.el
+++ snails-core.el
@@ -16,7 +16,7 @@
 ;;
 ;; Features that might be required by this library:
 ;;
-;; `cl-lib' `subr' `subr-x'
+;; `cl-lib' `subr-x'
 ;;
 
 ;;; This file is NOT part of GNU Emacs
@@ -163,7 +163,6 @@
 
 ;;; Require
 (require 'cl-lib)
-(require 'subr)
 (require 'subr-x)
 
 (when (featurep 'cocoa)
