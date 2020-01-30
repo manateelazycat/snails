5b9eae42049da9251bd3668b6195e01ff7857495
Andy Stewart <lazycat.manatee@gmail.com>
2020-01-30 23:58:39 +0800

    Put browser history in front of snails-default-backends.

--- snails-core.el
+++ snails-core.el
@@ -196,7 +196,7 @@
   :group 'snails)
 
 (defcustom snails-default-backends
-  '(snails-backend-awesome-tab-group snails-backend-buffer snails-backend-recentf snails-backend-eaf-pdf-table snails-backend-eaf-browser-history snails-backend-directory-files snails-backend-bookmark)
+  '(snails-backend-eaf-pdf-table snails-backend-eaf-browser-history snails-backend-awesome-tab-group snails-backend-buffer snails-backend-recentf snails-backend-directory-files snails-backend-bookmark)
   "The default backend"
   :type 'cons
   :group 'snails)
