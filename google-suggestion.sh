0734e511232869488a596e4c7970847403896677
xhcoding <xhcoding@163.com>
2020-03-30 18:41:42 +0800

    fix: convert &#39; to '

--- google-suggestion.sh
+++ google-suggestion.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-curl -x socks5h://localhost:1080 -sS "https://www.google.com/complete/search?client=hp&hl=en&xhr=t&q=${1}" | json_pp | sed -nE '/<\/?b>/{s```g;s`"|,|^ *``g;p}'
+curl -x socks5h://localhost:1080 -sS "https://www.google.com/complete/search?client=hp&hl=en&xhr=t&q=${1}" | json_pp | sed -nE '/<\/?b>/{s```g;s`"|,|^ *``g;p}'  | sed  "s/&#39;/'/g"
